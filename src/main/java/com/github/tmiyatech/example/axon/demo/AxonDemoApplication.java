79dc1fba77342625011f145b51f726f34d990616
Takeshi Miyajima <tmiyajima.sunarch@gmail.com>
2025-08-31 16:06:30 +0900

    initial commit

---
+++ src/main/java/com/github/tmiyatech/example/axon/demo/AxonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.tmiyatech.example.axon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonDemoApplication.class, args);
+	}
+
+}
